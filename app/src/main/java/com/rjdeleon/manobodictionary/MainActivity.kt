b66289c7883b9a22262a664ff73a622fa7c116c6
rjdeleon <ricardopablo.deleon@gmail.com>
2019-03-10 05:05:31 +0800

    Initial commit

---
+++ app/src/main/java/com/rjdeleon/manobodictionary/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rjdeleon.manobodictionary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
