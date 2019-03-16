27ad9e9445531707f6b56b117084a2f49f61c4e7
rjdeleon03 <ricardopablo.deleon@gmail.com>
2019-03-17 04:44:08 +0800

    Code cleanup

--- app/src/main/java/com/rjdeleon/manobodictionary/feature/MainActivity.kt
+++ app/src/main/java/com/rjdeleon/manobodictionary/feature/MainActivity.kt
@@ -3,21 +3,15 @@ package com.rjdeleon.manobodictionary.feature
 import android.content.Context
 import android.graphics.Rect
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.google.android.material.textfield.TextInputEditText
 import com.rjdeleon.manobodictionary.MainNavDirections
 import com.rjdeleon.manobodictionary.R
-import com.rjdeleon.manobodictionary.feature.main.MainSearchResultAdapter
-import com.rjdeleon.manobodictionary.feature.main.MainViewModel
-import com.rjdeleon.manobodictionary.feature.main.OnItemClickListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
