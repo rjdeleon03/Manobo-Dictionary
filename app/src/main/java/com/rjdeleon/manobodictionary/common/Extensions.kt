54af14386c3c332858ac8cd7d48a227270b06e1e
pabs <pabs@alink-group.co.jp>
2019-03-25 07:25:04 +0800

    Display message when entry list for a letter is empty

--- app/src/main/java/com/rjdeleon/manobodictionary/common/Extensions.kt
+++ app/src/main/java/com/rjdeleon/manobodictionary/common/Extensions.kt
@@ -1,11 +1,10 @@
 package com.rjdeleon.manobodictionary.common
 
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
 
-fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
+fun <T> LiveData<T>.observeOnce(observer: Observer<T>) {
     observeForever(object : Observer<T> {
         override fun onChanged(t: T?) {
             observer.onChanged(t)
